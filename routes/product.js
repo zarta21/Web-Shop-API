ed65b837ddc67852971a1a285dc5dba66470fdd7
Tautvydas <95254346+zarta21@users.noreply.github.com>
2023-01-07 14:21:12 +0100

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,22 +1,9 @@
-const router = require('express').Router(); //https://expressjs.com/en/guide/routing.html
+const router = require('express').Router(); 
 const Product = require('../models/Product');
 const { verifyTokenAndAdmin } = require('./verifyToken'); 
 
 
-//Endpoints:
-//GET - retrieves resources.
-//POST - submits new data to the server.
-//PUT - updates existing data.
-//DELETE - removes data. 
-//https://www.freecodecamp.org/news/rest-api-best-practices-rest-endpoint-design-examples/
-//https://stackoverflow.blog/2020/03/02/best-practices-for-rest-api-design/
-
-
-//CREATE:
-//************************************* */
 router.post('/', verifyTokenAndAdmin, async (req, res) => {
-    //req - what we get from user (name, email, input etc.)
-    //res - what we send for user
 
     const newProduct = new Product(req.body);
 
@@ -27,14 +14,9 @@ router.post('/', verifyTokenAndAdmin, async (req, res) => {
         res.status(500).json(err)
     }
 });
-//************************************* */
 
-
-//UPDATE:
-//************************************* */
 router.put('/:id', verifyTokenAndAdmin, async (req,res) => {
     try{
-        //to update product information use MongoDB function 'findByIdAndUpdate' and set new information from body:
         const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
             $set: req.body
         }, { new: true })
@@ -44,52 +26,39 @@ router.put('/:id', verifyTokenAndAdmin, async (req,res) => {
         res.status(500).json(err);
     }
 })
-//************************************* */
 
-
-//DELETE:
-//************************************* */
 router.delete('/:id', verifyTokenAndAdmin, async (req,res) => {
     try{
-        await Product.findByIdAndDelete(req.params.id) // find and delete product
+        await Product.findByIdAndDelete(req.params.id) 
 
         res.status(200).json('Product has been deleted')
     } catch(err) {
         res.status(500).json(err);
     }
 })
-//************************************* */
 
-
-//GET PRODUCT:
-//************************************* */
 router.get('/find/:id', async (req,res) => {
     try{
-        const product =  await Product.findByIdAnd(req.params.id) // find product by id
+        const product =  await Product.findByIdAnd(req.params.id) 
         res.status(200).json(product);
     } catch(err) {
         res.status(500).json(err);
     }
 })
-//************************************* */
 
-
-//GET ALL PRODUCTS:
-//************************************* */
 router.get('/', async (req,res) => {
-    const queryNewPrd = req.query.new; //create query to get only new products
-    const queryCategory = req.query.category; //create query to get products by category
+    const queryNewPrd = req.query.new; 
+    const queryCategory = req.query.category; 
 
     try{
         let products;
         
         if(queryNewPrd) {
-            //write a condition if there is query (.../new=true) send back 10 latest products:
             products = await Product.find().sort({ createdAt: -1 }).limit(10)
         } else if(queryCategory) {
             products = await Product.find({ categories: { $in: [queryCategory] } });
         } else {
-            products = await Product.find(); // send back all products
+            products = await Product.find();
         }
 
         res.status(200).json(products);
@@ -97,8 +66,5 @@ router.get('/', async (req,res) => {
         res.status(500).json(err);
     }
 })
-//************************************* */
 
-
-//exporting module:
 module.exports = router;
